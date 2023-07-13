c4eced77e250e4f7c33c5e7c70ac949143753d0a
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2023-07-13 19:22:45 -0400

    adds github action config

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe 'homepage' do
   it 'can load' do
     visit '/'
-    expect(page).to have_content('Hello World')
+    expect(page).to have_content('Hello Vite Rails')
   end
 end
